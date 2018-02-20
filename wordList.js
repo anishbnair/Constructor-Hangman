34770d531414d1678638563009502c282649c169
anishbnair <nbanish@gmail.com>
2018-02-20 12:26:45 -0500

    updated wordList.js file

---
+++ wordList.js
@@ -0,0 +1,11 @@
+
+// Create a list of words for game
+var wordList = [
+    'usa',
+    'india',
+    'japan',
+    'brazil'
+];
+
+// Export the list of words to other hangman script(s)
+module.exports = wordList;
